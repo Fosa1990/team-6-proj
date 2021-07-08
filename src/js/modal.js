bc1c1b5e6f34b9080db52258ee2c9230a33895a0
Fosa1990 <fosa1990@gmail.com>
2021-07-08 17:50:20 +0300

    Update modal.js
    add comments - testing

--- src/js/modal.js
+++ src/js/modal.js
@@ -3,9 +3,18 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    // mobileBtnOne = document.querySelector("[data-menu-one]"),
+    // mobileBtnTwo = document.querySelector("[data-menu-two]"),
+    // mobileBtnTree = document.querySelector("[data-menu-three]"), 
+    // mobileBtnFour = document.querySelector("[data-menu-four]"),
+    // mobileBtnClose = document.querySelector("[data-menu-close]"),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
+  // refs.mobileBtnOne.addEventListener('click', toggleModal);
+  // refs.mobileBtnTwo.addEventListener('click', toggleModal);
+  // refs.mobileBtnTree.addEventListener('click', toggleModal);
+  // refs.mobileBtnFour.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
